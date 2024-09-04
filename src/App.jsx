34dc9b6e64bb0eedd20796565e8e6e8c6b98996c
kachraz <kachraz@tuta.io>
2024-09-04 12:37:10 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
+import Header from "./co/Header.jsx";
+
 function App() {
-  return (
-    <h1>React Investment Calculator</h1>
-  )
+  return <h1>React PantySniffing</h1>;
+  <Header />;
 }
 
-export default App
+export default App;
---
+++ src/co/Header.jsx
@@ -0,0 +1,12 @@
+// Header Component
+
+import logo from "../assets/icl.png";
+
+export default function Header() {
+  return (
+    <header>
+      <img src={logo} alt="AssAndPussy" />
+      <h1>PantySniffing</h1>
+    </header>
+  );
+}
