161cc1397caaeaa9bbaa806251fcc07d595c4dca
kachraz <kachraz@tuta.io>
2024-09-04 19:59:37 +0530

    Add

--- src/co/Resu.jsx
+++ src/co/Resu.jsx
@@ -7,6 +7,10 @@ import { calInvRes, forma } from "../util/inso.js";
 
 export default function Resu({ input }) {
   const resultsData = calInvRes(input);
+  const initialInvestment =
+    resultsData[0].valueEndOfYear -
+    resultsData[0].interest -
+    resultsData[0].annualInvestment;
 
   console.log(resultsData);
 
@@ -23,13 +27,19 @@ export default function Resu({ input }) {
       </thead>
       <tbody>
         {resultsData.map((yD) => {
+          const totalInterest =
+            yD.valueEndOfYear -
+            yD.annualInvestment * yD.year -
+            initialInvestment;
+
+          const totalAmountInvested = yD.valueEndOfYear - totalInterest;
           return (
             <tr key={yD.year}>
               <td>{yD.year}</td>
               <td>{forma.format(yD.valueEndOfYear)}</td>
               <td>{forma.format(yD.interest)}</td>
-              <td></td>
-              <td></td>
+              <td>{forma.format(totalInterest)}</td>
+              <td>{forma.format(totalAmountInvested)}</td>
             </tr>
           );
         })}
