beef3469115a08a66a728150b5662d4b18409d89
kachraz <kachraz@tuta.io>
2024-09-04 18:35:07 +0530

    addResponsivePussy

--- src/co/Resu.jsx
+++ src/co/Resu.jsx
@@ -3,7 +3,37 @@ Results component -
 1. Where the calculations from input from Usin.jsx will be calculated using inso.js and output below the table
 */
 
+import { calInvRes, forma } from "../util/inso.js";
+
 export default function Resu({ input }) {
-  console.log(input);
-  return <p>RePANTYs</p>;
+  const resultsData = calInvRes(input);
+
+  console.log(resultsData);
+
+  return (
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>InvetmentValue</th>
+          <th>Interest(year)</th>
+          <th>TotalInterest</th>
+          <th>InvestedCapital</th>
+        </tr>
+      </thead>
+      <tbody>
+        {resultsData.map((yD) => {
+          return (
+            <tr key={yD.year}>
+              <td>{yD.year}</td>
+              <td>{forma.format(yD.valueEndOfYear)}</td>
+              <td>{forma.format(yD.interest)}</td>
+              <td></td>
+              <td></td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 }
--- src/util/inso.js
+++ src/util/inso.js
@@ -30,7 +30,7 @@ export function calInvRes({
 // The browser-provided Intl API is used to prepare a formatter object
 // This object offers a "format()" method that can be used to format numbers as currency
 // Example Usage: formatter.format(1000) => yields "$1,000"
-export const formatter = new Intl.NumberFormat("en-US", {
+export const forma = new Intl.NumberFormat("en-US", {
   style: "currency",
   currency: "USD",
   minimumFractionDigits: 0,
